13f54fef4046891cb3b11a984722c894a6650f25
DyegoSantosPereira0 <dycodedy@gmail.com>
2025-07-06 11:56:24 -0300

    Treino base

---
+++ treino001.py
@@ -0,0 +1,272 @@
+print("olá mundo!")
+
+nome = input("qual seu nome?")
+print("olá,", nome)
+
+idade = 25 
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("olá, mundo!")
+
+ome = input("qual seu nome?")
+
+print ("ola,", nome)
+
+idade = 25 
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("ola, mundo!")
+
+nome = input("qual seu nome?")
+print("ola,", nome)
+
+idade = 25 
+altura = 1.75
+ativo = True
+passivo = False 
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("ola, mundo!")
+nome = input("qual seu nome?")
+print("olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("ol´mundo!")
+
+nome = input("qual seu nome?")
+print("olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+print(type(ativo))
+print(type(mensagem))
+
+print("olá, mundo!")
+
+nome = input("qual seu nome?")
+print("olá,", nome)
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("olá, mundo!")
+
+nome = input("qual o seu nome?")
+print("olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("olá, mundo")
+
+nome = input("qual o seu nome?")
+print("olá,", nome)
+
+idade = 25 
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("olá mundo!")
+
+nome = input("Qual o seu nome?")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True 
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá mudo!")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá munso!")
+
+nome = input("Qual seu nome?")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá, mundo!")
+
+nome = input("Qual seu nome?")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá mundo! ")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25 
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá, mundo")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá mundo!")
+
+nome = input("Qual seu nome?")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+print(type(ativo))
+print(type(ativo))
+print(type(mensagem))
+
+print("Olá mundo!")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá mundo!")
+
+nome = input("qual seuy nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True 
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá mundo!")
+
+nome = input("qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
+print("Olá muindo!")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "oi!"
+
+print("Olá mundo!")
+
+nome = input("Qual seu nome?")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print("olá mundo!")
+
+nome = input("Qual seu nome? ")
+print("Olá,", nome)
+
+idade = 25
+altura = 1.75
+ativo = True
+mensagem = "Oi!"
+
+print(type(idade))
+print(type(altura))
+
