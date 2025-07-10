454415cc8ae32c5855a3173258bfd9b282091535
DyegoSantosPereira0 <dycodedy@gmail.com>
2025-07-10 17:25:46 -0300

    Add files via upload

---
+++ treino003.py
@@ -0,0 +1,667 @@
+for i in range(5):
+    print("repetiçao", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contado:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+    
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador +=1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contado = 0
+while contador < 5:
+    print("contado:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contando:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contador:", contador)
+    contador +=1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0
+while contador < 5:
+    print("contador:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contando:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contando:", contador)
+    contador += 1
+
+for i in range(5):
+    print("repetição", i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+contador = 0 
+while contador < 5:
+    print("contando:", contador)
+    contador += 1
+
+#-----------------------------------------------------------
+
+num = int(input("tabuada de: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+inicio = int(input("inicio: "))
+fim = int(input("fim: "))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar.")
+    opçao = input("escolha:")
+
+    if opçao == "1":
+        print("ola, mindo!")
+    elif opcao == "2":
+        print("voce escolher ajuda.")
+
+num - int(input("tabuada de: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+inicio = int(input("inicio::"))
+fim = int(input("fim: "))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares:", soma)
+
+opcao = "" 
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar")
+    opcao = input("escolha:")
+if opcao == "1":
+    print("ola. mundo!")
+elif opcao == "2":
+    print("voce escolhe ajuda")
+
+num = int(input("tabuada de: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+inicio = int(input("inicio: "))
+fim = int(input("fim: "))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar.")
+if opcao == 1:
+    print("ola, mundo")
+elif opcao == "2":
+    print("voce escolheu ajuda")
+
+num = int(input("tabuada de : "))
+for i in range(1, 11):
+    print(f" {num} x {i} = {num * i}")
+
+inicio = int(input("inicio:"))
+fim = int(input("fim"))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares:", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar.")
+if opcao == "1":
+    print("ola, mundo !")
+elif opcao == "2":
+    print("voce escolheu ajuda")
+
+num = int(input("tabuada de :"))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+inicio = int(input("inicio: "))
+fim = int(input("fim;"))
+som = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares:", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar.")
+    opcao = input("escolha")
+if opcao == "1":
+    print("ola, mundo!")
+elif opcao == "2":
+    print("voce escolheu ajuda")
+
+num = int(input("tabuada de: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+inicio = int(input("inicio:"))
+fim = int(input("fim:"))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares: ", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar.")
+    opcao = input("escolha:")
+if opcao == "1":
+    print("ola, mundo")
+elif opcao == "2":
+    print("voce escolheu ajuda")
+
+num = int(input("tabuada de:"))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+incio = int(input("incio:"))
+fim = int(input("fim:"))
+soma = 0
+
+for i in range(inicio, fim + 1):
+    if i % 2 == 0:
+        soma += i
+print("soma dos pares:", soma)
+
+opcao = ""
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digitar sair para encerrar.")
+    opcao = input("escolha")
+if opcao == "1":
+    print("ola, mundo")
+elif opcao == "2":
+    print("voce escolhe ajuda")
+
+num = int(input("tabuada de: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+incio = int(input("inicio: "))
+fim = int(input("fim"))
+soma = 0
+
+for i in range(incio, fim + 1):
+    if i % 2 == 0 :
+        soma += i
+print("soma dos pares", soma)
+
+opcao = "" 
+
+while opcao != "sair":
+    print("1 - ola")
+    print("2 - ajuda")
+    print("digite sair para encerrar")
+    opcao = input("escolha")
+if opcao == "1":
+    print("ola, mundo!")
+elif opcao == "2":
+    print("voce escolhe ajuda")
+
+import random
+
+secreto =random.randint("1, 100")
+tentativas = 0
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    tentativas += 1
+
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto!")
+    else:
+        print(f"parabens! acertou rm {tentativas} tentativas")
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0 
+chute = 0
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    tentativas += 1
+
+if chute < secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito alto!")
+else:
+    print(f"parabnes! acertou em {tentativas} tentativas.")
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    tentativas += 1
+
+if chute < secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito alto!")
+else:
+    print(f"parabens acertou em {tentativas} tentativas")
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite:"))
+    tentativas += 1 
+
+if chute < secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito alto!")
+else:
+    print(f"parabens! acertou em {tentativas} tentativas")
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0 
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+ 
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    tentativas += 1
+
+if chute < secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito alto!")
+else:
+    print(f'parabens! acertou em {tentativas} tentativas')
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0
+chute = 0
+
+print("adivinhe o numeor entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    
+if chute < secreto:
+    print("muito baixo!")
+elif chute > secreto:
+    print("muito alto!")
+else:
+    print(f"parabens! acertou em {tentativas} tentativas")
+
+import random
+
+secreto = random.randint(1, 100)
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    tentativas += 1
+
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto")
+    else:
+        print(f"parabens! acertou em {tentativas} tentativas")
+
+import random 
+secreto = random.randint(1, 100)
+tentativas = 0 
+chute = 0 
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+    
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto!")
+    else:
+        print(f"parabens acertou em {tentativas} tentaivas")
+
+import random
+secreto = random.randint(1, 100)
+tentativas = 0 
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite"))
+    tentativas +=1
+
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto!")
+    else:
+        print(f"parabens acertou em {tentativas} tentativas")
+
+import random
+
+secreto = random.randint(1, 100)
+tentativas = 0
+chute = 0 
+
+while chute != secreto:
+    chute = int(input("seu palpite: "))
+
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto!")
+    else: 
+        print(f"parabens! acertou em {tentativas} tentativas")
+
+import random 
+
+secreto = random.randint(1, 100)
+tentativas = 0
+chute = 0
+
+print("adivinhe o numero entre 1 e 100")
+
+while chute != secreto:
+     chute = int(input("seu palpite: "))
+     tentativas += 1
+
+     if chute < secreto:
+         print("muito baixo!")
+     elif chute > secreto:
+         print(f"parabens! acertou em {tentativas} tentativas")
+
+import random 
+
+secreto = random.randint(1, 100)
+tentativas = 0 
+chute = 0
+
+print("adivinhe o numeor entre 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu palpite:"))
+    tentativas += 1
+
+    if chute < secreto:
+        print("muito baixo!")
+    elif chute > secreto:
+        print("muito alto!")
+    else:
+        print(f"parabens ! acerou em {tentativas} tentivas")
+
+import random
+
+saecreto = random.randint(1,100)
+tentaivas = 0
+chute = 0
+
+print("adivinhe o numero 1 e 100")
+
+while chute != secreto:
+    chute = int(input("seu papilte:"))
+    tentativas += 1
+
+    if chute < secreto:
+        print("muito  baixo")
+
+    elif chute > secreto:
+        print("muito alto!")
+    else:
+        print(f"parabens! acertou em {tentativas} tentativas")
+ 
+ #-----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
