439037633a08a81c206713157620feca3cf5ad34
DyegoSantosPereira0 <dycodedy@gmail.com>
2025-07-11 19:23:30 -0300

    Add files via upload

---
+++ treino004.py
@@ -0,0 +1,680 @@
+frutas = ["maça", "banana", "uva"]
+print(frutas[0])
+print(frutas[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+frutas.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas.append("laranja")
+frutas.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower)
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas = ["maça", "banana", "uva"]
+print(frutas[0]) #maça
+print(frutas[-1]) #uva
+print(frutas [1:3]) #["banana", "uva"]
+
+frutas.append("laranja")
+frutas.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+fruta = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(fruta[1:3])
+
+fruta.append("laranja")
+fruta.remove("banana")
+frutas.sort()
+print(fruta)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print (fruta[1:3])
+
+frutas.append("laranja")
+fruta.remove("banana")
+fruta.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto [0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+fruta = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(fruta[1:3])
+
+fruta.append("laranja")
+fruta.remove("banana")
+fruta.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.loweer())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+fruta = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+frutas.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper)
+print(texto.lower)
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas = ["maça", "banana", "uva"]
+print(fruta[0])
+print(frutas[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+fruta.remove("banana")
+fruta.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[-1])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas = ["maça", "banana", "uva"]
+print(frutas[0])
+print(frutas[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+fruta.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+fruta = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+frutas.remove("banana")
+frutas.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas =["maça", "banana", "uva"]
+print(frutas[0])
+print(fruta[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+frutas.remove("banana")
+fruta.sort()
+print(frutas)
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+fruta = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(fruta[1:3])
+
+texto = "python é legal"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+frutas = ["maça", "banana", "uva"]
+print(fruta[0])
+print(fruta[-1])
+print(frutas[1:3])
+
+frutas.append("laranja")
+fruta.remove("banana")
+fruta.sort()
+print(fruta)
+
+texto = "python é legaç"
+print(texto[0])
+print(texto[-1])
+print(texto[0:6])
+
+print(texto.upper())
+print(texto.lower())
+print(texto.replace("legal", "incrivel"))
+print(texto.split())
+
+numero = [5, 8, 2, 10]
+media = sum(numero) / len(numero)
+print("media:", media)
+
+frase = input("digite uma frase")
+palavras = frase.split()
+print("palavra:", palavras)
+
+nome = ["ana", "bia", "carios"]
+for nome in nome:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input('digite uma frase:')
+palavras = frase.split()
+print("palavra:", palavras)
+
+nomes = ["ana", "bia", "carios"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+meida = sum(numeros) / len(numero)
+print("meida:", media)
+
+frase = input("digite uma frase: ")
+palavras = frase.split()
+print("palavra:", palavras)
+
+nomes = ["ana", "bia", "carios"]
+for nome in nomes:
+    print("ola,", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase: ")
+palavra = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carios"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numero)
+print("meida:", media)
+
+frase = input("digite uma frase:")
+palavra = frase.split()
+print("palavra:", palavras)
+
+nomes = ["ana", "bia", "carios"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numero)
+print("media", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carios"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase: ")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("Olá", nome)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numeros)
+
+print("meida:", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nome = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5,8,2,10]
+media = sum(numero) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5,8,2,10]
+media = sum(numeros) / len(numero)
+print("media", meida)
+
+frase = input("digite uma frase:")
+palvras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+    
+numeros = [5,8,2,10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase:")
+palvras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5,8,2,10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavra)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5, 8, 2, 10]
+meida = sum(numero) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase: ")
+palavras = frase.split()
+print("palavra:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5,8,2,10]
+meida = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavras)
+
+numeros = [5, 8, 2, 10]
+media = sum(numeros) / len(numeros)
+print("meida:", media)
+
+frase = input("digite uma frase:")
+palvras = frase.split()
+print("palavras:", palavras)
+
+nomes = [ "ana", "bia", "carlos"]
+for nome in nome:
+    print('ola', nome)
+
+numeros = [5,8,2,10]
+meida = sum(numeros) / len(numeros)
+print("media", media)
+
+frase = input("digite uma frase")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+numeros = [5,8,2,10]
+media = sum(numeros) / len(numeros)
+print("media:", media)
+
+frase = input("digite uma frase:")
+palavras = frase.split()
+print("palavras:", palavras)
+
+nomes = ["ana", "bia", "carlos"]
+for nome in nomes:
+    print("ola", nome)
+
+lista = []
+
+while True:
+    print("1 - adicionar item")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = ("escolha:")
+
+    if opcao == "1":
+        item = input("item a adicionar:")
+        lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover:")
+        if item in lista:
+            lista.remove(item)
+            print("removido:", item)
+        else:
+            print("item não encontrado.")
+    elif opcao == "3":
+        print("lista de compra:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opçao invalida")
+
+lista = []
+
+while True:
+    print("1 - adicionar item")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opçao = input("escolha: ")
+
+    if opcao == "1":
+       item = input("item a adicionar:")
+       lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover:")
+        if item in lista:
+            lista.remove(item)
+        else:
+            print("removido:", item)
+    elif opcao == "3":
+        print("lista de compras:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opçao invalida.")
+
+
+while True:
+    print("1 - adicionar")
+    print("2 - romover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha: ")
+
+    if opcao == "1":
+        item = input("item a adicionar")
+        lista.append(item)
+    elif opcao == "2":
+        if item in lista:
+            lista.remove(item)
+            print("removido:", item)
+        else:
+            print("item não encontrado.")
+    elif opcao == "3":
+        print("lista de compras:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opção invalida.")
+
+lista = []
+
+while True:
+    print("1 - adicionar item")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha:")
+    if opcao == "1":
+        item = input("item a adicionar:")
+        list.append(item)
+    elif opcao == "2":
+        item = input("item a remover: ")
+        if item in lista:
+            lista.remove(item)
+            print("removido:", item)
+        else:
+            print("removido:", item)
+    elif opcao == "3":
+        print("lista de compras:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opção invalida.")
+
+lista = []
+
+while True:
+    print("1 = adicionar")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha")
+
+    if opcao == "1":
+        item = input("item a adicionar:")
+        lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover")
+        if item in lista:
+            list.remove(item)
+            print("removido:", item)
+        else:
+            print("item não encontrado.")
+    elif opcao == "3":
+        print("lista de compras:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opçao invalidas.")
+
+lista = []
+
+while True:
+    print("1 - adicionar item")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha:")
+
+    if opcao == "1":
+        item = input("item a adicionar")
+        lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover:")
+        if item in lista:
+            lista.remove(item)
+            print("removido:", item)
+        else:
+            print("item não encontrado.")
+    elif opcao == "3":
+        print("lista de compras:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opçao invalida.")
+
+lista = []
+
+while True:
+    print("1 - adiconar item")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha:")
+
+    if opcao == "1":
+        item = input("item a adicionar:")
+        lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover: ")
+        if item in lista:
+            lista.remove(item)
+            print("item não encontrado")
+    elif opcao == "3":
+        print("lista de compra:", lista)
+    elif opcao == "4":
+        break
+    else:
+        print("opcao invalida.")
+
+    lista = []
+
+    while True:
+        print("1 - adicionar item")
+        print("2 - remover item")
+        print("3 - mostrar lista")
+        print("4 - sair")
+        opcao = input("escolha:")
+
+        if opcao == "1":
+            item = input("item a adicionar:")
+            lista.append(item)
+        elif opcao == "2":
+            item = input("item a remover:")
+            if item in list:
+                lista.remove(item)
+            else:
+                print("iem não encontrado")
+        elif opcao == "3":
+            print("lista de compras:", lista)
+        elif opcao == "4":
+            break
+        else:
+            print("opçao invalida.")
+
+lista = []
+
+while True:
+    print("1 - adicionar")
+    print("2 - remover item")
+    print("3 - mostrar lista")
+    print("4 - sair")
+    opcao = input("escolha:")
+
+    if opcao == "1":
+        item = input("item a adicionar: ")
+        lista.append(item)
+    elif opcao == "2":
+        item = input("item a remover:")
+        if item in lista:
+            lista.remove(item)
+            print("removido", item)
+        else:
+            print("item não encontrado")
+    elif opcao == "3":
+        print("lista de compras:")
+    elif opcao == "4":
+        break
+else:
+    print("opcao invalida")
+
+
+lista = []
+
+while True:
+
+
+
