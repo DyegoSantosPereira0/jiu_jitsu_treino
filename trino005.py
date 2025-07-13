9ae7f4754a0330c8a39a90f4f81a826c18c8765e
DyegoSantosPereira0 <dycodedy@gmail.com>
2025-07-12 22:03:49 -0300

    Add files via upload

---
+++ trino005.py
@@ -0,0 +1,628 @@
+aluno ={
+    "nome": "lucas", 
+    "idade": 20, 
+    "nota":8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print("aluno")
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+aluno = {
+    "nome": "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aproovado"] = True
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+numeros = {1, 2, 3, 2, 1}
+print(numeros)
+
+numeros.add(4)
+numeros.remove(2)
+
+aluno = {
+    "nome": "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+numeros = {1, 2, 3, 2, 1}
+print(numeros)
+
+numeros.add(4)
+numeros.remove(2)
+
+aluno = {
+    "nome": "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print(aluno)
+
+aluno = {
+    "nome": "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+aluno = {
+    "nome": "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print(aluno)
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+numeros = {1, 2, 3, 2, 1}
+print(numeros)
+
+numeros.add(4)
+numeros.remove(2)
+
+aluno = {
+    "nome" : "lucas",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+aluno = {
+    "nome": "lucas",
+    "iade": 20,
+    "nota": 8.5
+}
+
+print(aluno["nome"])
+aluno["nota"] = 9.0
+aluno["aprovado"] = True
+
+print(aluno)
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+
+numeros = {1, 2, 3, 2, 1}
+print(numeros)
+numeros.add(4)
+numeros.remover(2)
+
+#--------------------------------------------------------
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+     "idade": 30
+}
+
+print("nome", pessoa['nome'])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade:", pessoa['idade'])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+print("nomne:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+     "iadae": 30
+}
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoas = {"nome": "ana", "idade:": 30}
+
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoas = {
+    "nome": "ana",
+    "iadade": 30
+}
+
+print("frutas")
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoas = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa ["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", 'laranja'}
+print(frutas)
+
+pessoa = {
+    "nome:":"ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["name"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça","laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça","banana","maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+print("nome:", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça","laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana", 
+    "idade": 30
+}
+
+print("nome", pessoa["nome"])
+print("idade:", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+pessoa = {
+    "nome": "ana",
+    "idade": 30
+}
+
+print("nome:", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça","banana","maça","laranja"}
+print(frutas)
+
+pessoa = {
+    "nome":"ana", 
+    "idade": 30
+
+}
+print("nome", pessoa["nome"])
+print("idade", pessoa["idade"])
+
+frutas = {"maça", "banana", "maça", "laranja"}
+print(frutas)
+
+#--------------------------------------------------------------------------
+
+produto = {
+    "nome": "celular",
+    "preco": 1500.00,
+    "estoque": 10 
+}
+
+produto["preco"] *= 0.9
+print(produto)
+
+alunos = {}
+
+for i in range(3):
+    nome = input("nome do aluno:")
+    nota = float(input("nota:"))
+    alunos[nome] = nota
+
+print("notas cadastradas:", alunos)
+
+numeros = [1,2,3,3,2,1,4,5]
+sem_repetidos = set(numeros)
+print(sem_repetidos)
+
+a = {1,2,3}
+b = {3, 4, 5}
+
+print("Uniao", a | b)
+print("intersecao:", a & b)
+print("diferença", a - b)
+
+produto = {
+    "nome:": "celular",
+    "preco": 1500.00 ,
+    "estoque": 10
+}
+produto["preco"] *= 0.9
+print(produto)
+
+alunos  = {}
+
+for i in range(3):
+    nome = input("nome do aluno")
+    nota = float(input("nota:"))
+    alunos[nome] = nota
+
+print("notas cadastradas:", alunos)
+
+numeros = [1, 2, 3, 3, 2, 1, 4, 5]
+sem_repetidos = set(numeros)
+print(sem_repetidos)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print("união:", a | b)
+print("interseção:", a & b)
+print("diferença", a - b)
+
+agenda = {}
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    print("5 - sair")
+    opcao = input("escolha: ")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone: ")
+        agenda[nome] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover:")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido.")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("bnuscar: ")
+        if nome in agenda:
+            print("não encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.items():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opcao inválida.")
+
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    print("5 - sair")
+    opcao == input("escolha: ")
+
+    if opcao == "1":
+        nome = input("nome")
+        telefone = input("telefone: ")
+        agenda[nome] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover:")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido.")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("buscar:")
+        if nome in agenda:
+            print("telefone:", agenda[nome])
+        else:
+            print("não encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.item():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opçao invalida.")
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    print("5 - sair")
+    opcao = input("escolha:")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone:")
+        agenda[agenda] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover:")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("buscar:")
+        if nome in agenda:
+            print("telefone", agenda[nome])
+        else:
+            print("não encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.items():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opcao invalida.")
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    print("5 - sair")
+    opcao == input("escolha: ")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone:")
+        agenda[nome] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido.")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("buscar")
+        if nome in agenda:
+            print("telefone:", agenda[nome])
+        else:
+            print("não encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.item():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opçao invalida.")
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    opcao = input("escolha: ")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone:")
+        agenda[nome] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover:")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido.")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("buscar:")
+        if nome in agenda:
+            print("telefone:", agenda[nome])
+        else:
+            print("nao encontrado")
+    elif opcao == "4":
+        for nome, tel in agenda.items():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opçao ivalida.")
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - listar agenda")
+    print("5 - sair")
+    opcao = input("escolha")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone:")
+        agenda[nome] = telefone
+    elif opcao == "2":
+        nome = input("nome para remover: ")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome = input("buscar: ")
+        if nome in agenda:
+            print("telefone", agenda[nome])
+        else:
+            print("nao encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.items():
+            print(f"{nome}:{tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opcao invalida.")
+
+while True:
+    print("1 - adicionar contato")
+    print("2 - remover contato")
+    print("3 - buscar contato")
+    print("4 - lista agenda")
+    print("5 - sair")
+    opcao = input("escolha:")
+
+    if opcao == "1":
+        nome = input("nome:")
+        telefone = input("telefone")
+        agenda[nome] =telefone
+    elif opcao == "2":
+        nome = input("nome para remover:")
+        if nome in agenda:
+            del agenda[nome]
+            print("removido.")
+        else:
+            print("contato não encontrado.")
+    elif opcao == "3":
+        nome= input("buscar:")
+        if nome in agenda:
+            print("telefone:", agenda[nome])
+        else:
+            print("nao encontrado.")
+    elif opcao == "4":
+        for nome, tel in agenda.items():
+            print(f"{nome}: {tel}")
+    elif opcao == "5":
+        break
+    else:
+        print("opcao invalida.")
